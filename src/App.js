ca41c598189db1b2bae31381e83ff12197134afa
shelleydm <36711327+shelleydm@users.noreply.github.com>
2022-01-02 14:09:30 +1000

    Deploy to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Coin from "./Coin";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        axios
+            .get(
+                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+            )
+            .then((res) => {
+                setCoins(res.data);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const filteredCoins = coins.filter(
+        (coin) =>
+            coin.name.toLowerCase().includes(search.toLowerCase()) ||
+            coin.symbol.toLowerCase().includes(search.toLowerCase())
+    );
+
+    return (
+        <div className="coin-app">
+            <h1 className="coin-app-title">
+                Crypto search<span className="blinker"></span>
+            </h1>
+            <div className="coin-search">
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="coin-input"
+                        onChange={handleChange}
+                    />
+                </form>
+            </div>
+
+            <div className="coin-container">
+                <div className="coin-row">
+                    <div className="coin">
+                        <h1>Coin</h1>
+                    </div>
+                    <div className="coin-data">
+                        <p className="coin-price bold">Price ($Aud)</p>
+                        <p className="coin-volume bold">Volume</p>
+
+                        <p className="coin-percent bold">Price change</p>
+
+                        <p className="coin-marketcap bold">Market cap</p>
+                    </div>
+                </div>
+            </div>
+
+            {filteredCoins.map((coin) => {
+                return (
+                    <Coin
+                        key={coin.id}
+                        name={coin.name}
+                        image={coin.image}
+                        symbol={coin.symbol}
+                        volume={coin.total_volume}
+                        price={coin.current_price}
+                        priceChange={coin.price_change_percentage_24h}
+                        marketCap={coin.market_cap}
+                    />
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Coin.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Coin.css";
+
+const Coin = ({ name, image, symbol, price, volume, priceChange, marketCap }) => {
+    return (
+        <div className="coin-container">
+            <div className="wave"></div>
+            <div className="coin-row">
+                <div className="coin">
+                    <img src={image} alt="crypto" />
+                    <h1>{name}</h1>
+                    <p className="coin-symbol">{symbol}</p>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price">${price}</p>
+                    <p className="coin-volume">${volume.toLocaleString()}</p>
+                    {priceChange < 0 ? (
+                        <p className="coin-percent red">{parseFloat(priceChange).toFixed(2)}%</p>
+                    ) : (
+                        <p className="coin-percent green">{parseFloat(priceChange).toFixed(2)}%</p>
+                    )}
+                    <p className="coin-marketcap">${marketCap.toLocaleString()}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Coin;
